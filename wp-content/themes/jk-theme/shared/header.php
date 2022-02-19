ad71709f51a8b91fb67cf8e660c4bd8c1def6104
aminekun90 <mohamed.amine.bouzahar@gmail.com>
2022-02-19 20:09:13 +0100

    Add php templates

--- wp-content/themes/jk-theme/shared/header.php
+++ wp-content/themes/jk-theme/shared/header.php
@@ -1,3 +1,6 @@
+<?php
+// do_action("get_header");
+?>
 <!DOCTYPE html>
 
 <html class="no-js" <?php language_attributes(); ?>>
@@ -5,7 +8,7 @@
 
     <meta charset="<?php bloginfo("charset"); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" >
-
+    <title><?php wp_title(); ?></title>
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
     <?php wp_head(); ?>
